f741aebc87020f71a0cac497ef2001ed8fc86d05
David Arinze <fivelanes72@gmail.com>
2024-06-25 09:34:03 +0100

    Setup and dependency Installation

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import multer from 'multer';
+import ffmpeg from 'fluent-ffmpeg';
+import path from 'path';
+
+const app = express()
+
+
+
+
+
+
+
+
+export default app;
